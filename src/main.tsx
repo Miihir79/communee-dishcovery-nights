9b5ebde69f43a5cc00adf1987968c825b640d31e
tanishabelkar <tanishabelkar@gmail.com>
2025-07-08 01:36:58 +0530

    resolve tailwind config error

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
+import "./index.css";
+
+import App from './App'
 import { createRoot } from 'react-dom/client'
-import App from './src/App'
 
 createRoot(document.getElementById("root")!).render(<App />);
